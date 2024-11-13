80c4739caeeddbd3191164fd426828d2aab834e8
Rutvikkumar <parmarrutvik1414@gmail.com>
2024-11-13 21:34:13 +0530

    error fix

--- Frontend/src/componets/Signup.jsx
+++ Frontend/src/componets/Signup.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import toast from "react-hot-toast";
-import axios from "axios";
 import Login from "./Login";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import toast from "react-hot-toast";
+
 
 function Signup() {
   const location = useLocation();
@@ -16,32 +17,32 @@ function Signup() {
   } = useForm();
 
   const onSubmit = async (data) => {
-    const userinfo = {
+    const userInfo = {
       fullname: data.fullname,
       email: data.email,
       password: data.password,
     };
     await axios
-      .post("https://bookbazaar-e1qc.onrender.com/user/signup", userinfo)
+      .post("https://bookbazaar-e1qc.onrender.com/user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
-          toast.success("Signup Successfully!");
+          toast.success("Signup Successfully");
           navigate(from, { replace: true });
         }
-
-        localStorage.setItem("Users", JSON.stringify(res.data));
+        localStorage.setItem("Users", JSON.stringify(res.data.user));
       })
-      .catch((error) => {
-        if (error.response) {
-          toast.error(error);
+      .catch((err) => {
+        if (err.response) {
+          console.log(err);
+          toast.error("Error: " + err.response.data.message);
         }
       });
   };
   return (
     <>
       <div className="flex h-screen items-center justify-center">
-        <div className="w-[600px]">
+        <div className=" w-[600px] ">
           <div className="modal-box">
             <form onSubmit={handleSubmit(onSubmit)} method="dialog">
               {/* if there is a button in form, it will close the modal */}
@@ -53,15 +54,13 @@ function Signup() {
               </Link>
 
               <h3 className="font-bold text-lg">Signup</h3>
-              <div>
-                {/* Login */}
+              <div className="mt-4 space-y-2">
                 <span>Name</span>
                 <br />
-
                 <input
                   type="text"
-                  placeholder="Enter Your Name"
-                  className="w-80 px-30 border rounded-md outline-none"
+                  placeholder="Enter your fullname"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
                   {...register("fullname", { required: true })}
                 />
                 <br />
@@ -71,15 +70,14 @@ function Signup() {
                   </span>
                 )}
               </div>
-              <div>
-                {/* Login */}
+              {/* Email */}
+              <div className="mt-4 space-y-2">
                 <span>Email</span>
                 <br />
-
                 <input
                   type="email"
-                  placeholder="Enter Your Email"
-                  className="w-80 px-30 border rounded-md outline-none"
+                  placeholder="Enter your email"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
                   {...register("email", { required: true })}
                 />
                 <br />
@@ -90,14 +88,13 @@ function Signup() {
                 )}
               </div>
               {/* Password */}
-              <div>
+              <div className="mt-4 space-y-2">
                 <span>Password</span>
                 <br />
-
                 <input
                   type="password"
-                  placeholder="Enter Your Password"
-                  className="w-80 px-30 border rounded-md outline-none"
+                  placeholder="Enter your password"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
                   {...register("password", { required: true })}
                 />
                 <br />
@@ -107,17 +104,13 @@ function Signup() {
                   </span>
                 )}
               </div>
-              {/* button */}
-
+              {/* Button */}
               <div className="flex justify-around mt-4">
-                <button
-                  className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200"
-                  onClick={onSubmit}
-                >
+                <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
                   Signup
                 </button>
                 <p className="text-xl">
-                  Have a Account?{" "}
+                  Have account?{" "}
                   <button
                     className="underline text-blue-500 cursor-pointer"
                     onClick={() =>
@@ -125,7 +118,7 @@ function Signup() {
                     }
                   >
                     Login
-                  </button>
+                  </button>{" "}
                   <Login />
                 </p>
               </div>
